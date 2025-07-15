3dba1c5eff800c02150c2a349f453218a46c31f7
ash <tkdgys1234@gmail.com>
2025-07-15 23:33:28 +0900

    쿼리문 수정

--- back/src/util/db.ts
+++ back/src/util/db.ts
@@ -10,9 +10,12 @@ export const pool = new Pool({
 // 서버가 완전히 실행된 후 pool 쿼리 실행
 (async () => {
   try {
-    const res = await pool.query(
-      "SELECT table_schema, table_name FROM information_schema.tables WHERE table_name = 'projects';"
-    );
+    const res = await pool.query(`
+  SELECT table_schema, table_name 
+  FROM information_schema.tables 
+  WHERE table_name = 'projects'
+  AND table_schema = 'apol_schema';
+`);
     console.log("서버에서 보는 projects 테이블:", res.rows);
   } catch (err) {
     console.error("테이블 조회 중 에러:", err);
