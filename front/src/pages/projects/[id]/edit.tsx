eacded1fc65ce8760c03b147f7ef87d6d56477f8
ash <tkdgys1234@gmail.com>
2025-07-15 21:41:26 +0900

    동적 import로 수정

--- front/src/pages/projects/[id]/edit.tsx
+++ front/src/pages/projects/[id]/edit.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { projectApi, Project, UpdateProjectData } from "../../../util/api";
-import ProjectForm from "../../../components/projects/ProjectForm";
 import { isAdminLoggedIn } from "../../../util/auth";
 import dynamic from "next/dynamic";
 
