20101eeb154472c6dfcad7a220327b36c00b8350
ash <tkdgys1234@gmail.com>
2025-07-15 23:46:05 +0900

    마이그레이션 수정

--- back/migrations/001_create_projects_table.js
+++ back/migrations/001_create_projects_table.js
@@ -1,26 +1,27 @@
-/* eslint-disable camelcase */
-
 const up = (pgm) => {
   // 스키마 생성
   pgm.createSchema("apol_schema", { ifNotExists: true });
 
   // 프로젝트 테이블 생성
-  pgm.createTable(["apol_schema", "projects"], {
-    id: { type: "serial", primaryKey: true },
-    title: { type: "varchar(255)", notNull: true },
-    summary: { type: "text" },
-    content: { type: "text" },
-    tags: { type: "text[]" },
-    thumbnail: { type: "varchar(255)" },
-    duration: { type: "text" },
-    created_at: { type: "timestamp", default: pgm.func("now()") },
-    updated_at: { type: "timestamp", default: pgm.func("now()") },
-  });
+  pgm.createTable(
+    { schema: "apol_schema", name: "projects" },
+    {
+      id: { type: "serial", primaryKey: true },
+      title: { type: "varchar(255)", notNull: true },
+      summary: { type: "text" },
+      content: { type: "text" },
+      tags: { type: "text[]" },
+      thumbnail: { type: "varchar(255)" },
+      duration: { type: "text" },
+      created_at: { type: "timestamp", default: pgm.func("now()") },
+      updated_at: { type: "timestamp", default: pgm.func("now()") },
+    }
+  );
 };
 
 const down = (pgm) => {
   // 테이블 삭제
-  pgm.dropTable(["apol_schema", "projects"]);
+  pgm.dropTable({ schema: "apol_schema", name: "projects" });
   // 스키마 삭제
   pgm.dropSchema("apol_schema");
 };
