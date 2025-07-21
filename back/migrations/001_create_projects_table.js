00333a41ef9f75bf2fea2f67525472b257e0b337
ash <tkdgys1234@gmail.com>
2025-07-22 01:25:27 +0900

    마이그레이션 파일 수정

--- back/migrations/001_create_projects_table.js
+++ back/migrations/001_create_projects_table.js
@@ -1,25 +1,24 @@
-exports.up = async (pgm) => {
+const up = async (pgm) => {
   await pgm.createSchema("apol_schema", { ifNotExists: true });
 
   await pgm.db.query(`
-  CREATE TABLE IF NOT EXISTS "apol_schema"."projects" (
-    id SERIAL PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL,
-    "summary" TEXT,
-    "contentPath" TEXT,
-    "tags" TEXT[],
-    "thumbnail" VARCHAR(255),
-    "duration" TEXT,
-    "createdAt" TIMESTAMP DEFAULT now(),
-    "updatedAt" TIMESTAMP DEFAULT now()
-  );
-`);
+    CREATE TABLE IF NOT EXISTS "apol_schema"."projects" (
+      id SERIAL PRIMARY KEY,
+      "title" VARCHAR(255) NOT NULL,
+      "summary" TEXT,
+      "contentPath" TEXT,
+      "tags" TEXT[],
+      "thumbnail" VARCHAR(255),
+      "duration" TEXT,
+      "createdAt" TIMESTAMP DEFAULT now(),
+      "updatedAt" TIMESTAMP DEFAULT now()
+    );
+  `);
 };
-const down = (pgm) => {
-  // 테이블 삭제
-  pgm.dropTable({ schema: "apol_schema", name: "projects" });
-  // 스키마 삭제
-  pgm.dropSchema("apol_schema");
+
+const down = async (pgm) => {
+  await pgm.dropTable({ schema: "apol_schema", name: "projects" });
+  await pgm.dropSchema("apol_schema");
 };
 
 module.exports = { up, down };
