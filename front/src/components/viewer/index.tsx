c1486640d02338855f9cc3fc5fed0d937505fdcd
ash <tkdgys1234@gmail.com>
2025-07-22 16:04:30 +0900

    뷰어 수정

--- front/src/components/viewer/index.tsx
+++ front/src/components/viewer/index.tsx
@@ -1,8 +1,13 @@
-"use client";
-
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 import { Viewer } from "@toast-ui/react-editor";
 
 export default function ToastViewer({ markdown }: { markdown: string }) {
-  return <Viewer initialValue={markdown} theme="dark" />;
+  return (
+    <Viewer
+      initialValue={markdown}
+      theme="dark"
+      height="auto"
+      usageStatistics={false}
+    />
+  );
 }
