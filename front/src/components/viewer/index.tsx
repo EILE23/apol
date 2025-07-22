c01fdd667195a43857751229accc980bfc5eeed1
ash <tkdgys1234@gmail.com>
2025-07-22 16:14:14 +0900

    .

--- front/src/components/viewer/index.tsx
+++ front/src/components/viewer/index.tsx
@@ -1,6 +1,6 @@
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 import { Viewer } from "@toast-ui/react-editor";
-
+//뷰어
 export default function ToastViewer({ markdown }: { markdown: string }) {
   return (
     <Viewer
